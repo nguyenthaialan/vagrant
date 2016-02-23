bbebb82c59bfd9534b47353a6fe5434c396317e9
Alan <nguyenthaialan@gmail.com>
2016-02-23 20:24:59 +0100

    Poprawki z konfiguracją MySQL

--- packages/mysql.sh
+++ packages/mysql.sh
@@ -4,3 +4,7 @@ debconf-set-selections <<< 'mysql-server mysql-server/root_password password roo
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 apt-get install -y mysql-server
+
+sed -i 's/^bind-address.*/bind-address = 0.0.0.0/g' my.cnf
+
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
