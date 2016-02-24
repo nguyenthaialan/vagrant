85c1bbbf238f6379a308e9c856bae4b9414afaad
Alan <nguyenthaialan@gmail.com>
2016-02-24 20:44:24 +0100

    SQLite

--- packages/php5.sh
+++ packages/php5.sh
@@ -4,5 +4,6 @@ apt-get install -y php5
 apt-get install -y php5-curl
 apt-get install -y php5-gd
 apt-get install -y php5-pgsql
+apt-get install -y php5-sqlite
 apt-get install -y php5-memcached
 apt-get install -y php-apc
